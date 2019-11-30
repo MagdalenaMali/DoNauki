c6688d593a5c78b127f4a99a6a6a99f19aee239f
MagdalenaMali <magda.maliszewska08@gmail.com>
2019-11-30 19:33:19 +0100

    xxx

---
+++ Lab1Zadanie1/Lab1Zadanie1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba1;
+            int liczba2;
+            int Dodawanie;
+            int Odejmowanie;
+            int mnozenie;
+            int dzielenie;
+            int modulo;
+
+            Console.WriteLine("Podaj pierwszą liczbę");
+            liczba1 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj druga liczbę");
+            liczba2 = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Liczba1: {0}", liczba1);
+            Console.WriteLine("Liczba2: {0}", liczba2);
+
+            Dodawanie = liczba1 + liczba2;
+            Console.WriteLine("Dodawanie: {0}", Dodawanie);
+
+            Odejmowanie = liczba1 - liczba2;
+            Console.WriteLine("Odejmowanie: {0}", Odejmowanie);
+
+            mnozenie = liczba1 * liczba2;
+            Console.WriteLine("mnozenie: {0}", mnozenie);
+
+            dzielenie = liczba1 / liczba2;
+            Console.WriteLine("dzielenie {0}", dzielenie);
+
+            modulo = liczba1 % liczba2;
+            Console.WriteLine("modulo {0}", modulo);
+
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
